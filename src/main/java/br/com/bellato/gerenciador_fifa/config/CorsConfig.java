7fa4641944a60dc9fb9eda60fcf3992e6166b40e
Thiago <tfbellato@hotmail.com>
2025-09-17 19:59:26 -0300

    chore: atualização CORS

--- src/main/java/br/com/bellato/gerenciador_fifa/config/CorsConfig.java
+++ src/main/java/br/com/bellato/gerenciador_fifa/config/CorsConfig.java
@@ -2,6 +2,7 @@ package br.com.bellato.gerenciador_fifa.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,25 +12,20 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    // Se não houver variável, usa localhost por padrão
+    @Value("${APP_CORS_ALLOWED_ORIGINS:http://localhost:3000}")
+    private String[] allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        // Permitir o domínio do seu frontend React
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-
-        // Permitir todos os métodos HTTP que você usa (GET, POST, PUT, DELETE, OPTIONS)
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        // Permitir todos os headers que o frontend pode enviar
         config.setAllowedHeaders(Arrays.asList("*"));
-
-        // Se precisar enviar cookies ou autenticação, true; se não, pode ser false
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        // Aplicar essa configuração para todas as rotas
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
